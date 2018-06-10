824d1d4da6dc959d6df998427f0e22af8f381c16
Administrator <2643747390@qq.com>
2018-06-10 18:31:36 +0800

    clean

--- src/main/java/com/third/fpaltform/rest/HardwareVersionManager.java
+++ src/main/java/com/third/fpaltform/rest/HardwareVersionManager.java
@@ -2,7 +2,6 @@ package com.third.fpaltform.rest;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +11,6 @@ import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
